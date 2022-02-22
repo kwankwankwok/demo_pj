14dbb38a11f66a654cbc61da9e3a595952b6eb9d
kwankwankwok <kwankwankwok@gmail.com>
2022-02-22 23:06:53 +0800

    update

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+const { env } = process;
+
+console.log(env);
+
 mongoose
     .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
     .catch(e => {
