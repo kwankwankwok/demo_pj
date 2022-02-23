66d0190764014e181dc145724c8ff3a0f5c663a7
kwankwankwok <kwankwankwok@gmail.com>
2022-02-23 21:32:32 +0800

    test commmit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'http://ec2-18-166-154-126.ap-east-1.compute.amazonaws.com:3000/api',
 })
+console.log('axios base api', api);
 
 export const insertHorse = payload => api.post(`/horse`, payload)
 export const getAllHorses = () => api.get(`/horses`)
