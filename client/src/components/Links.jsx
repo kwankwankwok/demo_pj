dff08b28c75505bd254c352ffc1514c97ff8b28a
kwankwankwok <kwankwankwok@gmail.com>
2022-02-24 17:59:46 +0800

    change title name

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -19,7 +19,7 @@ const Links = () => {
     return (
         <React.Fragment>
             <Link to="/" className="navbar-brand">
-                My first MERN Application
+                Demo simple CMS
             </Link>
             <Collapse>
                 <List>
