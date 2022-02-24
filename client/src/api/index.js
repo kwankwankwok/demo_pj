b2cd167eb64c96c382e4c6838ce717f1b3dad6bc
kwankwankwok <kwankwankwok@gmail.com>
2022-02-24 18:23:42 +0800

    change api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: '/api',
 })
 
 export const insertHorse = payload => api.post(`/horse`, payload)
